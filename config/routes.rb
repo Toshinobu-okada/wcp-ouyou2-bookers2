0d9c92ad05a0944cb440b502e499f79daa07d05f
Toshinobu-okada <okacchi_nobu@yahoo.co.jp>
2021-02-04 04:42:24 +0000

    [Update]応用課題３:コメント、いいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   get 'top' => 'home#top'
   get 'home/about' => 'home#about'
   
